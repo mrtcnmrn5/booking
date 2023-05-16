e83dccc6b8b20dd35e01c0df66c517785e346143
Mertcan Moran <mertcanmmoran@gmail.com>
2023-05-16 19:29:14 +0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { BrowserRouter,Route,Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import List from "./pages/List/List";
 import Hotel from "./pages/Hotel/Hotel";
 
 function App() {
   return (
-  <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Home/>}/>
-    <Route path="/hotels" element={<List/>}/>
-    <Route path="/hotels/:id" element={<Hotel/>}/>
-  </Routes>
-  </BrowserRouter>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/:id" element={<Hotel />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
